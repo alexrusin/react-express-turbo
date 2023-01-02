fe3cce6090ecd2ed87532eb7b40c9e0e3d46bc1f
Alex Rusin <alex@sunriseintegration.com>
2023-01-01 21:19:29 -0800

    frontend-authentication

--- apps/frontend/src/components/Dashboard/Dashboard.tsx
+++ apps/frontend/src/components/Dashboard/Dashboard.tsx
@@ -8,12 +8,38 @@ import {
   MediaQuery,
   Burger,
   useMantineTheme,
+  Button,
 } from "@mantine/core";
 import HeaderContent from "../HeaderContent";
+import withAuthentication from "../withAuthentication";
+import { AxiosError } from "axios";
+import { UseQueryResult, useQuery } from "react-query";
+import apiClient from "../../services/api-client";
 
-export default function Dashboard() {
+type Message = {
+  message: string;
+};
+
+function Dashboard() {
   const theme = useMantineTheme();
   const [opened, setOpened] = useState(false);
+  const [enabled, setEnabled] = useState(false);
+
+  const {
+    data: response,
+    isLoading,
+  }: UseQueryResult<{ data: Message }, AxiosError> = useQuery(
+    "/message",
+    () => apiClient.get("/message/John"),
+    {
+      enabled: enabled,
+    }
+  );
+
+  const getMessage = () => {
+    setEnabled(true);
+  };
+
   return (
     <AppShell
       styles={{
@@ -60,6 +86,12 @@ export default function Dashboard() {
       }
     >
       <Text>Resize app to see responsive navbar in action</Text>
+      <Button mt="2em" loading={isLoading} onClick={getMessage}>
+        Get Message
+      </Button>
+      <Text>{response?.data.message}</Text>
     </AppShell>
   );
 }
+
+export default withAuthentication(Dashboard);
--- apps/frontend/src/services/api-client.ts
+++ apps/frontend/src/services/api-client.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { getTokens } from "frontend-cookies";
+import { eraseCookie, getTokens } from "frontend-cookies";
+import useUserStore from "../store/user-store";
 
 const [access_token] = getTokens(["access_token"]);
 
@@ -16,5 +17,30 @@ if (access_token) {
 }
 
 client.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+
+client.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      useUserStore.setState({ id: undefined, name: undefined, loading: false });
+      eraseCookie("access_token");
+      delete client.defaults.headers.common["Authorization"];
+    }
+    return Promise.reject(error);
+  }
+);
+
+client
+  .get("/api/user")
+  .then((response) => {
+    const user = response.data;
+    user.loading = false;
+    useUserStore.setState(user);
+  })
+  .catch((error) => {
+    useUserStore.setState({ loading: false });
+  });
 
 export default client;
